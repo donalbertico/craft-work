6428fe43ee94682a6090cfcf6e3fc6af59b410a4
donalbertico <atates@udlanet.ec>
2017-05-08 02:44:23 -0500

    eddting service file on server

--- server/services.js
+++ server/services.js
@@ -1,39 +1,41 @@
 import { Meteor } from 'meteor/meteor';
 
-Accounts.onCreateUser(function (options, user) {
-    if (!user.services.facebook) {
-        user.profile = {
-          photo : "https://s3.us-east-2.amazonaws.com/craft-work/nouser.png"
-        };
-        user.craft = {};
-        return user;
-    }
-    user.profile = {
-      name : user.services.facebook.first_name,
-      lastName : user.services.facebook.last_name,
-      photo : user.services.facebook.picture,
+Meteor.startup(() => {
+  Accounts.onCreateUser(function (options, user) {
+      if (!user.services.facebook) {
+          user.profile = {
+            photo : "https://s3.us-east-2.amazonaws.com/craft-work/nouser.png"
+          };
+          user.craft = {};
+          return user;
+      }
+      user.profile = {
+        name : user.services.facebook.first_name,
+        lastName : user.services.facebook.last_name,
+        photo : user.services.facebook.picture,
 
-    }
-    user.emails = [{address: user.services.facebook.email , verified : true}];
-    return user;
-});
+      }
+      user.emails = [{address: user.services.facebook.email , verified : true}];
+      return user;
+  });
 
-Slingshot.createDirective("userPicUpolad", Slingshot.S3Storage, {
-  bucket: Meteor.settings.AWS_BUCKET,
-  acl: "public-read",
-  region : Meteor.settings.AWS_REGION,
-  AWSAccessKeyId : Meteor.settings.AWS_ID,
-  AWSSecretAccessKey : Meteor.settings.AWS_KEY,
-  allowedFileTypes: ["image/png", "image/jpeg"],
-  maxSize: 10 * 1024 * 1024,
-  authorize: function () {
-    if (!this.userId) {
-      var message = "Please login before posting files";
-      throw new Meteor.Error("Login Required", message);
+  Slingshot.createDirective("userPicUpolad", Slingshot.S3Storage, {
+    bucket: Meteor.settings.AWS_BUCKET,
+    acl: "public-read",
+    region : Meteor.settings.AWS_REGION,
+    AWSAccessKeyId : Meteor.settings.AWS_ID,
+    AWSSecretAccessKey : Meteor.settings.AWS_KEY,
+    allowedFileTypes: ["image/png", "image/jpeg"],
+    maxSize: 10 * 1024 * 1024,
+    authorize: function () {
+      if (!this.userId) {
+        var message = "Please login before posting files";
+        throw new Meteor.Error("Login Required", message);
+      }
+      return true;
+    },
+    key : function(file){
+      return 'profile-'+this.userId;
     }
-    return true;
-  },
-  key : function(file){
-    return 'profile-'+this.userId;
-  }
+  });  
 });
