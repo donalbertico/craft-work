9ed65ae2341094b099377b3f6af6d2635563cb67
donalbertico <atates@udlanet.ec>
2017-06-06 09:16:04 -0500

    keep on the stuff

--- client/js/post.js
+++ client/js/post.js
@@ -1,10 +1,12 @@
-Template.post.onRendered(function(){
+Template.post.onCreated(function(){
 	var post = this.data.post;
-	// $('<meta>', { property: 'og:type', content: 'website' }).appendTo('head');
-	// $('<meta>', { property: 'og:image', content: post.photos[0] }).appendTo('head');
-	// $('<meta>', { property: 'og:url', content: 'https://craft-work-staging.herokuapp.com/post/'+post._id }).appendTo('head');
-	// $('<meta>', { property: 'og:title', content: post.name }).appendTo('head');
-	// $('<meta>', { property: 'og:description', content: post.description}).appendTo('head');
+	Meteor.setTimeout(function(){
+		$('<meta>', { property: 'og:type', content: 'website' }).appendTo('head');
+		$('<meta>', { property: 'og:image', content: post.photos[0] }).appendTo('head');
+		$('<meta>', { property: 'og:url', content: 'https://craft-work-staging.herokuapp.com/post/'+post._id }).appendTo('head');
+		$('<meta>', { property: 'og:title', content: post.name }).appendTo('head');
+		$('<meta>', { property: 'og:description', content: post.description}).appendTo('head');
+	},1000);
 });
 
 Template.post.helpers({
