0c34e31a6f9119301949f57111c106d7132c9567
donalbertico <atates@udlanet.ec>
2017-09-05 13:32:58 -0500

    making posible to user to publish wiotuh an enterprise

--- client/js/post.js
+++ client/js/post.js
@@ -2,12 +2,20 @@ Template.post.helpers({
 	owner : function(){
 		var post = this.post;
 		if(!post)return;
-    	Meteor.subscribe('userProf',post.user);
+    Meteor.subscribe('userProf',post.user);
 		var user = Meteor.users.findOne({_id:post.user});
 		return user;
 	},
 	isProduct : function(){
 		if(this.post.type == 'p')return true;
 		return false;
+	},
+	ownerEmail : function(){
+		var post = this.post;
+		if(!post)return;
+		Meteor.subscribe('userProf',post.user);
+		var user = Meteor.users.findOne({_id:post.user});
+		console.log(user.emails);
+		return user.emails[0].address;
 	}
 });
